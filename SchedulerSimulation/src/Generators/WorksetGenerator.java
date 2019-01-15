c879777399ef046f1e31920274e568902e873d21
Weeks4397 <35846723+Weeks4397@users.noreply.github.com>
2019-01-15 11:39:05 -0500

    workset

---
+++ SchedulerSimulation/src/Generators/WorksetGenerator.java
@@ -0,0 +1,14 @@
+package Generators;
+import java.util.ArrayList;
+import Processes.process;
+
+public class WorksetGenerator {
+
+    public ArrayList<process> WorkSet = new ArrayList<process>();
+
+    public WorksetGenerator(){
+
+        
+    }
+
+}
