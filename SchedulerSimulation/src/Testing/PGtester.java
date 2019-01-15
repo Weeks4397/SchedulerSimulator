017b0455e159f2224f79bbf02985b7be6ce61d88
Weeks4397 <35846723+Weeks4397@users.noreply.github.com>
2019-01-14 21:29:25 -0500

    processgenorater testing

--- SchedulerSimulation/src/Testing/PGtester.java
+++ SchedulerSimulation/src/Testing/PGtester.java
@@ -1,7 +1,75 @@
 package Testing;
 
+import Generators.ProccessGenator;
+
 public class PGtester {
     public static void main(String arg[]){
+
+        for (int i = 0;  i <= 1000; i++) {
+
+            ProccessGenator PG1 = new ProccessGenator(1);
+            ProccessGenator PG2 = new ProccessGenator(2);
+            ProccessGenator PG3 = new ProccessGenator(3);
+            ProccessGenator PG4 = new ProccessGenator(4);
+
+            if (PG1.getprocessRunTime() > 75 || PG1.getprocessRunTime() <25){
+                System.out.println("should print out a number runtime for 25 to 75 not ");
+                System.out.println("RunTime: " + PG1.getprocessRunTime());
+                System.out.println(" ");
+            }
+            if(PG2.getprocessRunTime() > 600 || PG2.getprocessRunTime() < 200){
+                System.out.println("should print out a number runtime for 200 to 600 not ");
+                System.out.println("RunTime: " + PG2.getprocessRunTime());
+                System.out.println(" ");
+            }
+            if(PG3.getprocessRunTime() > 500 || PG3.getprocessRunTime() < 150){
+                System.out.println("should print out a number runtime for 150 to 500");
+                System.out.println("RunTime: " + PG2.getprocessRunTime());
+                System.out.println(" ");
+            }
+            if(PG4.getprocessRunTime() > 1000 || PG4.getprocessRunTime() < 400){
+                System.out.println("should print out a number runtime for 400 to 1000");
+                System.out.println("RunTime: " + PG2.getprocessRunTime());
+                System.out.println(" ");
+            }
+
+            if (PG1.getBlock().getR() == "A" || PG3.getBlock().getR() == "A"){
+                if (PG1.getBlock().getBT() > 100|| PG1.getBlock().getBT() <60 || PG3.getBlock().getBT() > 100|| PG3.getBlock().getBT() < 60){
+                    System.out.println("should print out a number Block runtime form 60 to 100 not");
+                    System.out.println("Block RunTime: " + PG1.getBlock().getBT()+  "   " + PG3.getBlock().getBT());
+                    System.out.println(" ");
+                }
+            }
+            if(PG1.getBlock().getR() == "B" || PG3.getBlock().getR() == "B"|| PG4.getBlock().getR() == "B"){
+                if (PG1.getBlock().getBT() > 125|| PG1.getBlock().getBT() <75 || PG4.getBlock().getBT() > 125|| PG4.getBlock().getBT() < 75|| PG3.getBlock().getBT() > 125|| PG3.getBlock().getBT() < 75){
+                    System.out.println("should print out a number Block runtime form 60 to 100 not");
+                    System.out.println("Block RunTime: " + PG1.getBlock().getBT() +  "   " + PG3.getBlock().getBT() + "   " + PG4.getBlock().getBT() );
+                    System.out.println(" ");
+                }
+            }
+            if(PG3.getBlock().getR() == "C" || PG4.getBlock().getR() == "C"){
+                if (PG3.getBlock().getBT() > 400|| PG3.getBlock().getBT() <100 || PG4.getBlock().getBT() > 400|| PG4.getBlock().getBT() < 100){
+                    System.out.println("should print out a number Block runtime form 60 to 100 not");
+                    System.out.println("Block RunTime: " +PG3.getBlock().getBT() + "   " + PG4.getBlock().getBT());
+                    System.out.println(" ");
+                }
+            }
+            if (PG2.getBlock().getR() != null){
+                System.out.println("Process 2 should not have a block");
+                System.out.println("blockType: " + PG2.getBlock().getR());
+                System.out.println(" ");
+            }
+            if (PG1.getBlock().getR() == "C" || PG4.getBlock().getR() == "A"){
+                System.out.println("Process 1 should not have RC and process 4 should not have RA");
+                System.out.println(" ");
+            }
+
+
+
+        }
+        System.out.println(" all done testing process Generator ");
+
+
 
     }
 }
