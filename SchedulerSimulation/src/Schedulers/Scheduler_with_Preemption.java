b5b6e3bae372a0297b5d973b33433e70da037295
Alec235 <alec.goncalves@student.shu.edu>
2019-01-28 16:09:06 -0500

    abstracting scheduler

--- SchedulerSimulation/src/Schedulers/Scheduler_with_Preemption.java
+++ SchedulerSimulation/src/Schedulers/Scheduler_with_Preemption.java
@@ -5,12 +5,12 @@ import java.util.List;
 //TODO: Don't know if LWC and SRT include timeout, need to find out
 /**
  * Scheduler_with_Preemption is a subclass of Scheduler_with_TimeOut.
- * This class extends Scheduler_with_TimeOut so that preemption is possible during each event.
+ * This class extends Scheduler_with_NoTimeOut so that preemption is possible during each event.
  * Preemption:  A process in the ReadyQ has higher priority to run than the currently running process.
  *              The running process will be preempted and placed back on ReadyQ so the higher priority
  *              process can run.
  */
-public abstract class Scheduler_with_Preemption extends Scheduler_with_TimeOut {
+public abstract class Scheduler_with_Preemption_NoTimeOut extends Scheduler_without_TimeOut {
 
     /**
      *Constructor for scheduler with Preemption
