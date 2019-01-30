e4c6fe06ca9fbb43f7d8d966e6cbde9de69ce591
weeks4397 <alexander.weeks@studnet.shu.edu>
2019-01-30 16:48:31 -0500

    added mor abstration in readyQ

--- SchedulerSimulation/src/ReadyQueue/SRT_Q.java
+++ SchedulerSimulation/src/ReadyQueue/SRT_Q.java
@@ -2,32 +2,11 @@ package ReadyQueue;
 
 import Processes.process;
 
-public class SRT_Q extends ReadyQ {
+public class SRT_Q extends PrioityQ{
 
     public SRT_Q(){
         super();
         PriQ = new MinHeap<process>(100, Comparators.By_SRT);
     }
-    @Override
-    public process peek() {
-        process peekVal = null;
-        peekVal =PriQ.peek();
-        return peekVal;
-    }
 
-    @Override
-    public process poll() {
-        process pollVal = null;
-        pollVal = PriQ.poll();
-        return pollVal;
-    }
-
-    @Override
-    public void add(process elem) {
-        PriQ.add(elem);
-    }
-
-    public Boolean isEmpty() {
-        return PriQ.isEmpty();
-    }
 }
