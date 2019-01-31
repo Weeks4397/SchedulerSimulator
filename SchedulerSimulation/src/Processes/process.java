07258feecd68201281be9973b268c81c3e8321b5
Alec235 <alec.goncalves@student.shu.edu>
2019-01-31 14:30:04 -0500

    Clone needs to be fixed

--- SchedulerSimulation/src/Processes/process.java
+++ SchedulerSimulation/src/Processes/process.java
@@ -337,4 +337,5 @@ public abstract class process {
                 this.getBlockServiceTime(), this.getBlockWaitTime(), this.getFinishTime());
        return theProcess;
     }
+
 }
--- SchedulerSimulation/src/Testing/FIFOtester.java
+++ SchedulerSimulation/src/Testing/FIFOtester.java
@@ -31,7 +31,6 @@ public class FIFOtester {
 
         //begin handling the events until end of simulation
         //When there are no more events, all events will be equal to MAXVAL
-        int I = 1;
         while (FIFO.getNextEvent() != Integer.MAX_VALUE) {
             FIFO.handleNextEvent();
         }
@@ -83,5 +82,48 @@ public class FIFOtester {
         System.out.println(Test.Workset.get(6));
         System.out.println();
         System.out.println(Test.Workset.get(7));
+
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println(FIFO.TheResources[0].getNumOfBlocks());
+        System.out.println(FIFO.TheResources[1].getNumOfBlocks());
+        System.out.println(FIFO.TheResources[2].getNumOfBlocks());
+        System.out.println();
+        System.out.println(FIFO.TheResources[0].getTotalBlockTime());
+        System.out.println(FIFO.TheResources[1].getTotalBlockTime());
+        System.out.println(FIFO.TheResources[2].getTotalBlockTime());
+        System.out.println();
+        System.out.println();
+        System.out.println();
+
+
+        Scheduler_FIFO FIFO1 = new Scheduler_FIFO(Test.Workset);
+
+        //Begin the algorithm
+
+        //P is the init process to get time with CPU
+        process P1 = FIFO1.ReadyProcesses.poll();
+        FIFO1.updateActiveProcess(P1);
+
+        //update all of the initial events
+        FIFO1.update_NextUnblock_and_Resource();
+        FIFO1.updateNextSchedExit();
+        FIFO1.updateNextArrival();
+        FIFO1.updateNextBlock();
+
+        //Get the next event out of these
+        FIFO1.updateNextEvent();
+
+
+        //begin handling the events until end of simulation
+        //When there are no more events, all events will be equal to MAXVAL
+        while (FIFO1.getNextEvent() != Integer.MAX_VALUE) {
+            FIFO1.handleNextEvent();
+        }
+
+        System.out.println("time: " + FIFO1.getTime());
+        System.out.println("Active time: " + FIFO1.getActiveTime());
+        System.out.println("Idle time: " + FIFO1.getIdleTime());
     }
 }
